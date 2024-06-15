d91feb445f1f1c5017b73d41c44c15da1f9a0afc
Hyun <hwn123h@naver.com>
2024-06-15 22:03:07 +0900

    네이버 크롤링, 렌더링

--- client/src/Header/header.jsx
+++ client/src/Header/header.jsx
@@ -2,20 +2,23 @@ import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import cgv_logo from "../assets/cgv.png";
 import './header.css'
+export const daytime = () => {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");
+    let day = date.getDate().toString().padStart(2, "0");
+
+    return { year, month, day };
+}
+
 function header() {
 
-    const daytime = () => {
-        let date = new Date();
-        let year = date.getFullYear();
-        let month = (date.getMonth() + 1).toString().padStart(2, "0");
-        let day = date.getDate().toString().padStart(2, "0");
 
-        return { year, month, day };
-    }
 
     const today = daytime();
 
 
+    let todaydate = `${today.year}${today.month}${today.day}`
 
     return (
         <Container fluid>
@@ -25,7 +28,7 @@ function header() {
                     <h3>{today.month}월{today.day}일</h3>
                 </Col>
                 <Col className="cgv">
-                    <a href={`http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=${today}`} style={{ width: 150 }}>
+                    <a href={`http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=${todaydate}`} style={{ width: 150 }}>
                         <img src={cgv_logo} alt="CGV Logo" className="header_img" />
                     </a>
                 </Col>
--- client/src/Time/Time.jsx
+++ client/src/Time/Time.jsx
@@ -1,83 +1,52 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import moment from 'moment';
+import { daytime } from '../Header/header';
 
 import './time.css'
 function Time() {
+    const today = daytime();
+
     const [selectedWeek, setSelectedWeek] = useState(null);
     //주4
-    const [inputTime4, setInputTime4] = useState({ hour: "", minute: "" });
+
     const [resultTime4, setResultTime4] = useState("");
+    const [resultTime4_2, setResultTime4_2] = useState("");
     //주2
-    const [inputTime2, setInputTime2] = useState({ hour: "", minute: "" });
     const [resultTime2, setResultTime2] = useState("");
+    const [resultTime2_2, setResultTime2_2] = useState("");
 
     const [time, setTime] = useState("");
 
-    const handleWeekSelect = (week) => {
-        setSelectedWeek(week);
-    }
 
-    const handleTimeChange = (event, week) => {
-        const { id, value } = event.target;
-        if (value.match(/^\d+$/)) {
-            if (week === 'week4') {
-                setInputTime4(prev => ({
-                    ...prev,
-                    [id]: value,
-                }));
-            } else {
-                setInputTime2(prev => ({
-                    ...prev,
-                    [id]: value,
-                }));
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000'); // 백엔드 서버로 요청
+            if (!response.ok) {
+                throw new Error('failed to fetch movie times');
             }
+            const data = await response.json(); // JSON 형태로 변환
+            setTime(data); // 시간 상태 업데이트
+
+            // 주/2와 주/4 선택 시 계산된 결과를 미리 설정
+            const [hour, minute] = data.split(":");
+            setResultTime4(calculateEndTime(parseInt(hour), parseInt(minute), 7, 50)); // 주/4 계산 (마감)
+            setResultTime4_2(calculateEndTime(parseInt(hour), parseInt(minute), 7, 20)); // 주/4 계산 (마감2)
+            setResultTime2(calculateEndTime(parseInt(hour), parseInt(minute), 7, 20)); // 주/2 계산 (마감)
+            setResultTime2_2(calculateEndTime(parseInt(hour), parseInt(minute), 6, 50)); // 주/2 계산 (마감2)
+
+        } catch (err) {
+            console.log(err); // 에러 처리
         }
     }
 
     const calculateEndTime = (hour, minute, hourDiff, minuteDiff) => {
         if (!hour || !minute) return "";
-        const inputMoment = moment({ hour, minute });
-        const result = inputMoment.subtract({ hours: hourDiff, minutes: minuteDiff });
-        return result.format("HH:mm");
+        const inputMoment = moment({ hour, minute }); // moment 객체 생성
+        const result = inputMoment.subtract({ hours: hourDiff, minutes: minuteDiff }); // 계산
+        return result.format("HH:mm"); // 시간 형식으로 포맷팅하여 반환
     }
 
-    const calculateTimeWeek4End = () => {
-        setResultTime4(calculateEndTime(inputTime4.hour, inputTime4.minute, 8, 0));
-    };
-
-    const calculateTimeWeek4End2 = () => {
-        setResultTime4(calculateEndTime(inputTime4.hour, inputTime4.minute, 7, 20));
-    };
-
-    const calculateTimeWeek2End = () => {
-        setResultTime2(calculateEndTime(inputTime2.hour, inputTime2.minute, 7, 30));
-    };
-
-    const calculateTimeWeek2End2 = () => {
-        setResultTime2(calculateEndTime(inputTime2.hour, inputTime2.minute, 6, 50));
-    };
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:8000');
-            if (!response.ok) {
-                throw new Error('failed to fetch movie times');
-            }
-            const data = await response.json();
-            setTime(data);
-
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    const handleGoBack = () => {
-        setSelectedWeek(null);
-        setInputTime2({ hour: "", minute: "" }); // 주 선택이 변경되면 입력도 초기화합니다.
-        setInputTime4({ hour: "", minute: "" });
-        setResultTime2(""); // 결과도 초기화합니다.
-        setResultTime4("");
-    }
 
     useEffect(() => {
         fetchData();
@@ -85,66 +54,34 @@ function Time() {
 
     return (
         <>
-
-            {!selectedWeek && (
-                <div className="time-card-content-header">
-                    <button onClick={() => handleWeekSelect('week2')}>주/2</button>
-                    <button onClick={() => handleWeekSelect('week4')}>주/4</button>
-                </div>
-            )}
-
-            <div className="time-card">
-                {selectedWeek === "week4" && (
-                    <>
-                        <div className='time-card-content-input'>
-                            <input type="text" className='Timeinput' placeholder='hour' id="hour" value={inputTime4.hour} onChange={(e) => handleTimeChange(e, 'week4')} required />
-                            <input type="text" className='Timeinput' placeholder='minute' id="minute" value={inputTime4.minute} onChange={(e) => handleTimeChange(e, 'week4')} required />
-                        </div>
-                        <div className='time-card-content-select'>
-                            <button onClick={calculateTimeWeek4End}>마감</button>
-                            <button onClick={calculateTimeWeek4End2}>마감2</button>
-                        </div>
-                        <div className="time-card-content-result">
-                            <p style={{ color: "white" }}>입력 시간</p>
-                            <p className="time" style={{ color: "black" }}>{inputTime4.hour}:{inputTime4.minute}</p>
-                            <p style={{ color: "white" }}>출근 시간</p>
-                            <p className="time" style={{ color: "black" }}>{resultTime4}</p>
-                        </div>
-                        {selectedWeek && (
-                            <button onClick={handleGoBack}>뒤로 가기</button>
-                        )}
-                    </>
-                )}
-                {selectedWeek === "week2" && (
-                    <>
-                        <div className='time-card-content-input'>
-                            <input type="text" className='Timeinput' placeholder='hour' id="hour" value={inputTime2.hour} onChange={(e) => handleTimeChange(e, 'week2')} required />
-                            <input type="text" className='Timeinput' placeholder='minute' id="minute" value={inputTime2.minute} onChange={(e) => handleTimeChange(e, 'week2')} required />
-                        </div>
-                        <div className='time-card-content-select'>
-                            <button onClick={calculateTimeWeek2End}>마감</button>
-                            <button onClick={calculateTimeWeek2End2}>마감2</button>
-                        </div>
-                        <div className="time-card-content-result">
-                            <p>
-                                입력 시간
-                            </p>
-                            <p className="time">{inputTime2.hour}:{inputTime2.minute}</p>
-                            <p>
-                                출근 시간
-                            </p>
-                            <p className="time">{resultTime2}</p>
-                        </div>
-                        {selectedWeek && (
-                            <button onClick={handleGoBack}>뒤로 가기</button>
-                        )}
-                    </>
-                )}
-            </div>
-
             <div className="movie-times">
-                <h2>CGV 영화 시간표</h2>
-                오늘 마지막 영화시간 {time}
+                <h2>{today.month}월 {today.day}일 </h2>
+                <h2>마지막 영화시간</h2>
+                <h3>{time}</h3>
+
+            </div>
+            <div className="time-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Part</th>
+                            <th>출근 시간 (마감)</th>
+                            <th>출근 시간 (마감2)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="week-row">
+                            <td>주/4</td>
+                            <td>{resultTime4}</td>
+                            <td>{resultTime4_2}</td>
+                        </tr>
+                        <tr className="week-row">
+                            <td>주/2</td>
+                            <td>{resultTime2}</td>
+                            <td>{resultTime2_2}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
